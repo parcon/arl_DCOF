de555e19295d06fc13249b77128c506003b98373
parcon <parker.conroy@gmail.com>
2012-12-06 10:41:04 -0700

    mouse bug fixes, mouse support added to DCOF cpp

--- src/DCOF_pcl.cpp
+++ src/DCOF_pcl.cpp
@@ -25,20 +25,29 @@ target_link_libraries(DCOF_pcl cereal_port) in Cmake
 #define TIMEOUT 1000
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
-
+float deg2rad= 0.0174532925;
 
 int main(int argc, char** argv)
 {
   ros::init (argc, argv, "publish_DCOFS");
   ros::Rate loop_rate(500);
   ros::NodeHandle node;
-  ros::Publisher pub = node.advertise<PointCloud> ("custom_laser_scaner", 1);
-  ros::Publisher mouse_pub = node.advertise<Vector3> ("mouse_commands", 1);
+  ros::Publisher cloud_pub = node.advertise<PointCloud> ("custom_laser_scaner", 1);
+  ros::Publisher mouse_pub = node.advertise<geometry_msgs::Vector3> ("mouse_commands", 1);
+  
+  
   
   cereal::CerealPort device;
   char serial_data[REPLY_SIZE];
   char * pEnd;
-  long int enc1, enc2, dist;
+  double theta_y, theta_x, x, y, z;
+  double x_old, y_old, z_old;
+  x_old=y_old=z_old= 0.0;
+  double dist, x1,y1,squal1, x2, y2, squal2, pwm1, pwm2, enc1, enc2; //X1;Y1;SQUAL1;X2;Y2;SQUAL2;PWM1;PWM2;QEI1;QEI2
+float enc1_max=1000;//encoder counts from one side to another
+float enc2_max=1000;
+float degree_x=120;//degrees of throw
+float degree_y=120;
   geometry_msgs::Vector3 mouse_msg;
 
   try{ device.open(SERIAL_PORT, SERIAL_SPEED); }
@@ -50,9 +59,9 @@ int main(int argc, char** argv)
     ROS_INFO("The serial port is opened.");
 
 
-  PointCloud::Ptr msg (new PointCloud);
-  msg->header.frame_id = "Laser_Frame";
-  msg->height = msg->width = 1;
+	PointCloud::Ptr msg (new PointCloud);
+	msg->header.frame_id = "/DCOF_wo_rot";
+//  msg->height = msg->width = 1;
 
   while (node.ok() && ros::ok())//while ROS and node are working
   {
@@ -62,39 +71,51 @@ int main(int argc, char** argv)
             ROS_ERROR("Serial Timeout!");
         }
         ROS_INFO("Serial Message: %s", serial_data);
-/*
-Extract dist and encoders from serial message
+	
+		//Extract data and encoders from serial message
+		//X1;Y1;SQUAL1;X2;Y2;SQUAL2;PWM1;PWM2;QEI1;QEI2
+		x1 = strtol (serial_data,&pEnd,10);
+		y1 = strtol (pEnd,&pEnd,10);
+		squal1 = strtol (pEnd,&pEnd,10);
+		x2 = strtol (pEnd,&pEnd,10);
+		y2 = strtol (pEnd,&pEnd,10);
+		squal2 = strtol (pEnd,&pEnd,10);
+		pwm1 = strtol (pEnd,&pEnd,10);
+		pwm2 = strtol (pEnd,&pEnd,10);
+		enc1 = strtol (pEnd,&pEnd,10);
+		enc2 = strtol (pEnd,NULL,10);
+	//	dist = strtol (pEnd,NULL,10);
 
-*/ 
-  enc1 = strtol (serial_data,&pEnd,10);
-  enc2 = strtol (pEnd,&pEnd,10);
-  dist = strtol (pEnd,NULL,10);
-   
+		ROS_INFO("Decoded Message: %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", x1,y1,squal1,x2,y2,squal2,pwm1,pwm2,enc1,enc2);
 
+		//Tri for where point is
+		dist=x1-x2*y1-y2; //NEEDS fixing
+		theta_x=deg2rad*(enc1-enc1_max/2)*(degree_x/enc1_max);
+		theta_y=deg2rad*(enc2-enc2_max/2)*(degree_y/enc2_max);
+		
+		y=dist*sin(theta_y);
+		x=dist*sin(theta_x);
+		z=dist*cos(theta_y);
 
-/*
-This section will include the math that takes the encoder values and distance from the DCOF 
-and outputs the point in x,y,z
+		//y=y_old;
+		//x=x_old;
+		z=z_old;
 
-as encoders proportinal to angle
-*/
-y=dist*sin(theta_y);
-x=dist*sin(theta_x);
-z=dist*cos(theta_y);
+		ROS_INFO("Gimbal orientation~ x:%f	y: %f", theta_x,theta_y);
+		ROS_INFO("Hand Position~ x:%f	y:%f	z:%f", x, y, z);
+		ROS_INFO("Mouse Movements~ x:%f	y:%f	z:%f", x1, y1, z-z_old);
+		
+		msg->points.assign(1,pcl::PointXYZ(x, y, z));
+		msg->header.stamp = ros::Time::now ();
+		cloud_pub.publish (msg);
 
-  
-   msg->points.push_back (pcl::PointXYZ(1.0, 2.0, 3.0));
-   msg->header.stamp = ros::Time::now ();
-   pub.publish (msg);
+		mouse_msg.x=x1;
+		mouse_msg.y=y2;
+		mouse_msg.z=z-z_old;
+		mouse_pub.publish(mouse_msg);
 
-mouse_msg.x=x;
-mouse_msg.y=y;
-mouse_msg.z=z;
-mouse_pub.publish(mouse_msg);
-
-
-  ros::spinOnce ();
-  loop_rate.sleep ();
+		ros::spinOnce ();
+		loop_rate.sleep ();
 }//while
 }//main
 
--- src/mouse_control.cpp
+++ src/mouse_control.cpp
@@ -1,3 +1,18 @@
+/*
+Parker Conroy
+ARLab @ University of Utah
+
+This program listens for a vector3 on the mouse_control topic, for the purpose of moving the cursor.
+
+If it is not working:
+sudo modprobe uinput
+sudo chmod 755 /dev/uinput
+
+OR
+
+change /dev/uinput to /dev/inputs/uinput
+*/
+
 #include <ros/ros.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +24,6 @@
 #include <linux/uinput.h>
 #include <geometry_msgs/Vector3.h>
 
-
 #define EV_KEY          0x01
 #define EV_REL          0x02
 #define EV_ABS          0x03
@@ -18,27 +32,33 @@ int dx,dy,dz;
 
 void mouse_cb(const geometry_msgs::Vector3& msg)
 {
-  msg.x=dx;
-  msg.y=dy;
-  msg.z=dz;
+  dx=msg.x;
+  dy=msg.y;
+  dz=msg.z;
 }
 
 
 int main(int argc, char** argv)
 {
-  int fd;
-  ros::init (argc, argv, "Mouse Control");
-  ros::Rate loop_rate(500);
+	int fd;
+	//int rc;
+	//unsigned char i;
+	struct uinput_user_dev uidev;
+	struct input_event ev;
+        
+  ros::init (argc, argv, "Mouse_Control");
   ros::NodeHandle node;
-  ros::Subscriber sub = node.subscribe("mouse_control", 10, mouse_cb)
+  ros::Subscriber sub = node.subscribe("mouse_control", 10, mouse_cb);
+  ros::Rate loop_rate(500);
 
+system( "rosservice  call /ardrone/setledanimation 5 2 5" );
 
-
-fd = open("/dev/input/uinput", O_WRONLY | O_NONBLOCK);
+fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
 if(fd < 0) {
     ROS_ERROR("Mouse uinput error 1");
     exit(0);
-
+		}
+		
 if(ioctl(fd, UI_SET_EVBIT, EV_REL) < 0)
        ROS_ERROR("Mouse uinput error 2");
     if(ioctl(fd, UI_SET_RELBIT, REL_X) < 0)
@@ -47,21 +67,21 @@ if(ioctl(fd, UI_SET_EVBIT, EV_REL) < 0)
         ROS_ERROR("Mouse uinput error 2");
         
 memset(&uidev, 0, sizeof(uidev));
-    snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "DCOF-mouse_control");
-    uidev.id.bustype = BUS_USB;
-    uidev.id.vendor  = 0x1;
-    uidev.id.product = 0x1;
-    uidev.id.version = 1;
+snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "DCOF-mouse_control");
+uidev.id.bustype = BUS_USB;
+uidev.id.vendor  = 0x1;
+uidev.id.product = 0x1;
+uidev.id.version = 1;
 
 if(write(fd, &uidev, sizeof(uidev)) < 0)
         ROS_ERROR("Mouse uinput error 3");
 
-    if(ioctl(fd, UI_DEV_CREATE) < 0)
+if(ioctl(fd, UI_DEV_CREATE) < 0)
        ROS_ERROR("Mouse uinput error 3");
 
-    sleep(2);
-    
-    while (node.ok() && ros::ok()) {//while ROS and node are working
+sleep(2);
+ROS_INFO("Starting Mouse Control");    
+while (node.ok() && ros::ok()) {//while ROS and node are working
 
  memset(&ev, 0, sizeof(struct input_event));
 ev.type = EV_REL;
@@ -86,9 +106,9 @@ if(write(fd, &ev, sizeof(struct input_event)) < 0)
 ROS_ERROR("Mouse syn");
 */
 ros::spinOnce ();
-  loop_rate.sleep ();
+loop_rate.sleep ();
 
 }//while ros ok
-ret = ioctl(fd, UI_DEV_DESTROY); //destroys mouse input
+ioctl(fd, UI_DEV_DESTROY); //destroys mouse input
 ROS_ERROR("Mouse control or ROS crashed");
 }//main
